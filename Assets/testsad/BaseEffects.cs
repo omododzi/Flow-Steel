c0817f0649a16f8182a75fbc72fabd4dae62d859
omododzi <83458772+omododzi@users.noreply.github.com>
2026-03-03 20:16:50 +0300

    test

---
+++ Assets/testsad/BaseEffects.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseEffects")]
+public class BaseEffects : ScriptableObject
+{
+    public float time_working;
+    public float damage_per_sec;
+}
---
+++ Assets/testsad/Fire.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Fire: MonoBehaviour
+{
+    public BaseEffects fire;
+
+
+    public void How_Fire_work()
+    {
+        
+    }
+}
